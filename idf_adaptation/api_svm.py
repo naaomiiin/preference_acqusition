65b3bd30f38033f794fc6ef9cf09c78dbdb1ebab
naaomiiin <naoming.s2@gmail.com>
2015-11-18 11:52:24 +0900

    パラメータから３つの値取得

--- idf_adaptation/api_svm.py
+++ idf_adaptation/api_svm.py
@@ -1,15 +1,25 @@
 # -*- coding:utf-8 -*-
+
 from bottle import route, run, HTTPResponse, request, Bottle, response, static_file
 import simplejson as json
-import datetime
 from StringIO import StringIO
+import svm_predict as s
 
 app = Bottle()
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name = "unknownさん"):
-    return "こんにちは！" + name
+#@app.route('/', method="GET")
+#def tmp():
+#    return "TEST!!!"
 
+@app.route('/svm', method="GET")
+def svm():
+    idfValue    = str(request.query["idf-value"])
+    knownList   = request.query["known-list"]
+    unknownList = request.query["unknown-list"]
+#    print type(knownList)
+#    return "|>"+idfValue+":"+float(knownList)
+    #パラメータを型を変更して読み込む　でぐぐってみる
+    return s.svm( knownList, unknownList, idfValue )
+    
 
-run(app,host='0.0.0.0', port=9900, debug=True, reloader=True)
+run(app,host='0.0.0.0', port=9900, debug=True, reloader=True) # ビルドインサーバの実行
--- idf_adaptation/svm_predict.py
+++ idf_adaptation/svm_predict.py
@@ -1,27 +1,35 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
 from sklearn.svm import LinearSVC
 import numpy as np
 
+def svm(knownList,unknownList,idfValue):
+    # 学習データ
+    print str(knownList)+" => "+str(unknownList)+" => "+str(idfValue)
+#    for x in knownList:
+    print len(knownList)
+#    print ">>>"+len(knownList)
+#known,unknown配列個数変数に入れる
 
-# 学習データ
-data_training_tmp = np.loadtxt('data_training.txt', delimiter=' ')
-data_training = [[x[1]] for x in data_training_tmp] # IDF値
-label_training = [int(x[0]) for x in data_training_tmp] # わかる(1), わからない(0)
 
-# 試験データ
-#data_test = np.loadtxt('CodeIQ_mycoins.txt', delimiter=' ')
-data_test = 4.442
 
+# 1 for x in range(10) 1が10回表示される
+
+#    data_training = [[x[1]] for x in data_training_tmp] # IDF値
+#    label_training = [int(x[0]) for x in data_training_tmp] # わかる(1), わからない(0)
+    
+    # 試験データ
+#data_test = 3.521
+
+   
 # 学習
-estimator = LinearSVC(C=1.0)  # モデルを定義
-estimator.fit(data_training, label_training)  # fit関数を使って教師データからパラメータを推定
-
+ #   estimator = LinearSVC(C=1.0)  # モデルを定義
+ #   estimator.fit(data_training, label_training)  # fit関数を使って教師データからパラメータを推定
+    
 # 予測
-label_prediction = estimator.predict(data_test)
-if label_prediction == [1]:
-    print data_test,"はわかる"
-else:
-    print data_test,"はわからない"
+  #  label_prediction = estimator.predict()
+  #  if label_prediction == [1]:
+  #      return data_test + "はわかる"
+  #  else:
+  #      return data_test + "はわからない"
 
-#print(label_prediction)
