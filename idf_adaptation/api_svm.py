29c6907b2764e3b87214caae63a554dd71534b34
naaomiiin <naoming.s2@gmail.com>
2015-11-22 18:16:40 +0900

    svmAPI作成完了

--- idf_adaptation/api_svm.py
+++ idf_adaptation/api_svm.py
@@ -16,14 +16,22 @@ app = Bottle()
 @app.route('/svm', method="GET")
 def svm():
     idfValue    = str(request.query["idf-value"])
-    knownList   = request.query["known-list"]
-    knownList = ast.literal_eval(knownList)
-    unknownList = request.query["unknown-list"]
-    unknownList = ast.literal_eval(unknownList)
+    knownString   = request.query["known-list"]
+    knownList = []
+    for idf in knownString.split(","):
+        idf = float(idf)
+        knownList.append([idf])
+#   knownList = ast.literal_eval(knownList)
+    unknownString = request.query["unknown-list"]
+#   unknownList = ast.literal_eval(unknownList)
+    unknownList =[]
+    for idf in unknownString.split(","):
+        idf = float(idf)
+        unknownList.append([idf])
     #print type(knownList)
 #    return "|>"+idfValue+":"+float(knownList)
     #パラメータを型を変更して読み込む　でぐぐってみる
-    return s.svm( knownList, unknownList, idfValue )
+    return str(s.svm( knownList, unknownList, idfValue ))
 
 
 run(app,host='0.0.0.0', port=9900, debug=True, reloader=True) # ビルドインサーバの実行
--- idf_adaptation/svm_predict.py
+++ idf_adaptation/svm_predict.py
@@ -2,37 +2,48 @@
 
 from sklearn.svm import LinearSVC
 import numpy as np
+import sys
 
 def svm(knownList,unknownList,idfValue):
-    # 学習データ
-    # print knownList
-    # for i in range(len(knownList)):  # lenでリストの要素数を求める, rangeにループ回数(回数分の要素のリストが戻り値), inはリストをとる
-    # print knownList[i]
 
+# 学習データ
     knownList_count = len(knownList)
     unknownList_count = len(unknownList)
-    print "わかるIDF個数>>>", knownList_count , ", わからないIDF個数>>>" , unknownList_count
-  
-    for i in range(knownList_count):     # 1がわかるIDF個数回表示
-        print ("1")
-    for i in range(unknownList_count):     # 1がわかるIDF個数回表示                                                                                                                 
-        print ("0")
+    #print "わかるIDF個数>>>", knownList_count , ", わからないIDF個数>>>" , unknownList_count
 
-#    data_training = [[x[1]] for x in data_training_tmp] # IDF値
+    idfValue = float(idfValue)
+    print type(idfValue)
+
+    data_training = knownList + unknownList   # IDF値結合
+    print data_training
+    
+    knownArray = []
+    for i in range(knownList_count):     # "1" わかるIDF個数回表示
+        knownArray.append(1)
+    
+    unknownArray = []
+    for i in range(unknownList_count):     # "0" わからないIDF個数回表示                                                                                                            
+        unknownArray.append(0)
+
+    label_training = knownArray + unknownArray  # リスト結合
+    print label_training
+
+#    data_training  = [[x[1]] for x in data_training_tmp] # IDF値
 #    label_training = [int(x[0]) for x in data_training_tmp] # わかる(1), わからない(0)
+
     
     # 試験データ
 #data_test = 3.521
 
    
 # 学習
- #   estimator = LinearSVC(C=1.0)  # モデルを定義
- #   estimator.fit(data_training, label_training)  # fit関数を使って教師データからパラメータを推定
+    estimator = LinearSVC(C=1.0)  # モデルを定義
+    estimator.fit(data_training, label_training)  # fit関数を使って教師データからパラメータを推定
     
 # 予測
-  #  label_prediction = estimator.predict()
-  #  if label_prediction == [1]:
-  #      return idfValue , "はわかる"
-  #  else:
-  #      return idfValue , "はわからない"
+    label_prediction = estimator.predict([[idfValue]])
+    if label_prediction == [1]:
+        return 1  # わかる
+    else:
+        return 0  # わからない
 
